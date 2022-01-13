302bd72d6c015b0c63b5c6196503989642dbb629
Lucia Pereira <luhelenapereira@gmail.com>
2022-01-13 14:44:43 -0300

    refatora load e uso de imagem para o asset

--- js/AssetManager.js
+++ js/AssetManager.js
@@ -2,6 +2,16 @@ export default class AssetManager{
     constructor(){
         this.aCarregar = 0;
         this.carregadas = 0;
+        this.imagens = new Map();
+    }
+    carregaImagem(chave, source){
+        const img1 = new Image();
+        img1.src= source;
+        this.imagens.set(chave, img1);
+    }
+
+    img(chave){
+        return this.imagens.get(chave);
     }
     progresso(){
         if(this.aCarregar>0){
