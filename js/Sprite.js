78b6fcd80f291c0b12c5c7df5f28cde6596f8210
Lucia Pereira <luhelenapereira@gmail.com>
2022-01-18 12:30:18 -0300

    adiciona restricao de moviemto a direita

--- js/Sprite.js
+++ js/Sprite.js
@@ -30,6 +30,7 @@ export default class Sprite{
         this.y = this.y + this.vy*dt;
         this.mx = Math.floor(this.x/this.cena.mapa.SIZE);
         this.my = Math.floor(this.y/this.cena.mapa.SIZE);
+        this.aplicaRestricoes(dt);
     }
     colidiuCom(outro){
         return !(
@@ -38,5 +39,26 @@ export default class Sprite{
             (this.y - this.h/2 > outro.y + outro.h/2) || 
             (this.y + this.h/2 < outro.y - outro.h/2)
         );
+    }
+    aplicaRestricoes(dt){
+        const SIZE = this.cena.mapa.SIZE;
+        if(this.vx>0){
+            const pmx = this.mx + 1;
+            const pmy = this.my;
+            if(this.cena.mapa.tiles[pmy][pmx] != 0){
+                const tile = {
+                    x:pmx*SIZE + SIZE/2, 
+                    y:pmy*SIZE + SIZE/2,  
+                    w:SIZE, 
+                    h:SIZE
+                };
+                this.cena.ctx.strokeStyle = "write";
+                this.cena.ctx.strokeRect(tile.x - SIZE/2, tile.y-SIZE/2, SIZE,SIZE)
+                if(this.colidiuCom(tile)){
+                    this.vx =0;
+                    this.x = tile.x - tile.w/2 - this.w/2 -1;
+                }
+            }
+        }
     }
 }
