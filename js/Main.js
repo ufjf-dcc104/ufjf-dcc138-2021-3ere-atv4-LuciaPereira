a14343067b5f4eb6bc519d80360fb3376a25c9f2
Lucia Pereira <luhelenapereira@gmail.com>
2022-01-21 00:04:45 -0300

    Cria gerenciador de entrada de controle

--- js/Main.js
+++ js/Main.js
@@ -3,8 +3,10 @@ import Cena from "./Cena.js"
 import Mapa from "./Mapa.js";
 import Mixer from "./Mixer.js";
 import Sprite from "./Sprite.js";
-import modeloMapa1 from "./maps/mapa1.js";
+import modeloMapa1 from "../maps/mapa1.js";
+import InputManager from "./InputManager.js";
 
+const input = new InputManager();
 const mixer = new Mixer(10);
 const assets = new AssetManager(mixer);
 
@@ -19,10 +21,15 @@ const canvas = document.querySelector("canvas");
 canvas.width = 14*32;
 canvas.height = 10*32;
 
+input.configurarTeclado({
+    "ArrowLeft" : "MOVE_ESQUERDA",
+    "ArrowRight" : "MOVE_DIREITA",
+});
+
 const cena1 = new Cena(canvas, assets);
 
 const mapa1 = new Mapa(10,14,32);
-mapa1.configuraMapa(modeloMapa1);
+mapa1.carregaMapa(modeloMapa1);
 cena1.configuraMapa(mapa1);
 
 const pc = new Sprite({ x:50,y:150, vx:10});
